b0bb8ae848143002c96afbeaec37d69eb5f015c7
Juan Carlos Custodio <juan.custodio204075@potros.itson.edu.mx>
2021-03-08 07:26:24 -0700

    Layouts

---
+++ PopCornFactory/app/src/main/java/custodio/juancarlos/popcornfactory/DetallePelicula.kt
@@ -0,0 +1,14 @@
+package custodio.juancarlos.popcornfactory
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetallePelicula : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detalle_pelicula)
+
+
+
+    }
+}
---
+++ PopCornFactory/app/src/main/java/custodio/juancarlos/popcornfactory/Pelicula.kt
@@ -0,0 +1,3 @@
+package custodio.juancarlos.popcornfactory
+
+class Pelicula (var titulo: String, var img: Int, var header: Int, var descripcion: String) {}
